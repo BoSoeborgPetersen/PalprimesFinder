1d3363f11eabbc0dfbc6c64014f8b42c35d92fec
Bo Petersen <Bo Petersen>
2017-05-21 16:23:09 +0200

    Add project files.

---
+++ PalprimesFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PalprimesFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
